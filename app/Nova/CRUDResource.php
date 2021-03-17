885fac6fde7b581f69ca9ca535d6c0fea66f8088
Khaled AbdElRahim <khaled710017@gmail.com>
2021-03-18 01:33:07 +0200

    [nova-crud-itmes] Create resource for CRUD

---
+++ app/Nova/CRUDResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+
+abstract class CRUDResource extends Resource
+{
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name_ar';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name_ar',
+        'name_en',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make('Arabic Name', 'name_ar'),
+
+            Text::make('English Name', 'name_en'),
+        ];
+    }
+}
