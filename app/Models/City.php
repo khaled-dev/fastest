1af77edcd64b457bd76bf42b24b6a48af7d5a7e4
Khaled AbdElRahim <khaled710017@gmail.com>
2021-03-26 20:43:14 +0200

    [auth] Add scope to city

--- app/Models/City.php
+++ app/Models/City.php
@@ -16,7 +16,6 @@ class City extends Model
      */
     protected $fillable = ['name_ar', 'name_en'];
 
-
     /**
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -24,5 +23,17 @@ class City extends Model
     public function territory(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Territory::class);
+    }
+
+    /**
+     * Get all Cities with the given territory-id
+     *
+     * @param $query
+     * @param $territory_id
+     * @return mixed
+     */
+    public function scopeForTerritoryId($query, $territory_id)
+    {
+        return $query->where('territory_id', $territory_id);
     }
 }
