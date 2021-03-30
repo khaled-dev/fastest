71b06b1f502ebcce0d199418282bce9e1c5d4606
Khaled AbdElRahim <khaled710017@gmail.com>
2021-03-30 03:36:54 +0200

    [update-record] Show update-record-courior in nova

--- app/Models/CourierUpdateRequest.php
+++ app/Models/CourierUpdateRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CourierUpdateRequest extends Model
 {
@@ -26,6 +26,16 @@ class CourierUpdateRequest extends Model
         'car_number',
         'iban_number',
     ];
+
+    /**
+     * Get the courier
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function courier(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Courier::class);
+    }
 
     /**
      * Get the territory
---
+++ app/Nova/CourierUpdateRequest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\BelongsTo;
+
+class CourierUpdateRequest extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\CourierUpdateRequest::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            BelongsTo::make('Courier', 'courier')->showOnIndex(),
+
+            Text::make('Name', 'name')->sortable(),
+
+            Text::make('National ID', 'national_number')->sortable(),
+
+            Select::make('Gender', 'gender')
+                ->options([
+                    'male' => 'Male',
+                    'female' => 'Female',
+                ])
+                ->displayUsingLabels()
+                ->sortable(),
+
+            Text::make('IBAN Nnumber', 'iban_number')->hideFromIndex(),
+
+            Text::make('Car Number', 'car_number')->hideFromIndex(),
+
+            BelongsTo::make('Nationality', 'nationality')->hideFromIndex(),
+
+            BelongsTo::make('Bank', 'bank')->hideFromIndex(),
+
+            BelongsTo::make('Territory', 'territory')->hideFromIndex(),
+
+            BelongsTo::make('City', 'city'),
+
+            BelongsTo::make('Car Type', 'car_type')->hideFromIndex(),
+        ];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Determine if the current user can delete the given resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public function authorizedToDelete(Request $request)
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the current user can create new resources.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public static function authorizedToCreate(Request $request)
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the current user can update the given resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public function authorizedToUpdate(Request $request): bool
+    {
+        return false;
+    }
+}
