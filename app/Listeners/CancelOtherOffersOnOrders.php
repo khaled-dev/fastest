1cc0506c2b34db35a80a738566d90a7fae38561a
Khaled AbdElRahim <khaled710017@gmail.com>
2021-06-05 21:54:21 +0200

    [offers] Add Offer accept even/listener

---
+++ app/Listeners/CancelOtherOffersOnOrders.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Offer;
+use App\Events\OfferAccepted;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CancelOtherOffersOnOrders implements shouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * Handle the event.
+     *
+     * @param  OfferAccepted  $event
+     * @return void
+     */
+    public function handle(OfferAccepted $event)
+    {
+        $otherOffers = $event->offer->order->offers()->otherOffers($event->offer);
+
+        $otherOffers->update([
+            'state' => Offer::CANCELED,
+        ]);
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\OfferAccepted;
+use App\Listeners\CancelOtherOffersOnOrders;
 use App\Models\Order;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
@@ -19,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        OfferAccepted::class => [
+            CancelOtherOffersOnOrders::class,
         ],
     ];
 
