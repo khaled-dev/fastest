9a9718b158fe502e92098c63821d3e6046b84ec2
Khaled AbdElRahim <khaled710017@gmail.com>
2021-06-18 21:30:44 +0200

    [fcm] Add Ar locales for notifications

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'offers' => [
+        'title' => 'You have received an offer',
+        'body' => 'New offer received for your order',
+    ]
+];
