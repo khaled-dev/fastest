a416281e2cff65b655c89db673848c2e755346b6
Khaled AbdElRahim <khaled710017@gmail.com>
2021-06-20 00:01:49 +0200

    [fcm] Update place offer notification

--- app/Listeners/SendPlacedOfferNotification.php
+++ app/Listeners/SendPlacedOfferNotification.php
@@ -3,31 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\OfferPlaced;
-use App\Models\Customer;
-use App\Models\Offer;
-use App\Models\Order;
-use App\Services\Contracts\ICloudMessaging;
-use App\Services\FirebaseCloudMessaging\FirebaseCloudMessaging;
-use App\Services\Logic\NotificationService;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use App\Services\Logic\NotificationService;
 
 class SendPlacedOfferNotification
 {
-
-    /**
-     * cloudMessaging Object.
-     *
-     * @var ICloudMessaging
-     */
-    protected ICloudMessaging $cloudMessaging;
-
-    public function __construct(ICloudMessaging $cloudMessaging)
-    {
-        $this->cloudMessaging = $cloudMessaging;
-    }
-
     /**
      * Handle the event.
      *
@@ -36,28 +16,20 @@ class SendPlacedOfferNotification
      */
     public function handle(OfferPlaced $event)
     {
-        /** @var Offer $offer */
-        /** @var Order $order */
-        /** @var Customer $customer */
         $offer    = $event->offer;
         $order    = $event->offer->order;
         $customer = $order->customer;
 
         $notification = [
-            'title' => __('notifications.offers.placed.title'),
-            'body' => __('notifications.offers.placed.body'),
+            'title'     => __('notifications.offers.placed.title'),
+            'body'      => __('notifications.offers.placed.body'),
             'image_url' => $offer->courier->profile_picture,
         ];
 
         NotificationService::saveNotification($customer, $notification);
 
         if ($token = $customer->notificationToken) {
-            // send notification
-            $this->cloudMessaging
-                ->withToken($token)
-                ->withNotification($notification)
-                ->send();
-
+            NotificationService::pushNotification($token, $notification);
             return;
         }
 
