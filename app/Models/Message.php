0b62b9286e1f095abf225ef6deaadf12b257a8b6
Khaled AbdElRahim <khaled710017@gmail.com>
2021-06-25 15:10:44 +0200

    [chat] Add message mfsm

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'offer_id',
+        'body',
+    ];
+
+    /**
+     * Get the message model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function messages(): \Illuminate\Database\Eloquent\Relations\MorphTo
+    {
+        return $this->morphTo();
+    }
+}
