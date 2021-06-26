10345034d42dd68ec055e52400391225db6f6459
Khaled AbdElRahim <khaled710017@gmail.com>
2021-06-26 19:05:24 +0200

    [nova-crud-2] Add location with map

---
+++ app/Nova/Location.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\BelongsTo;
+use GeneaLabs\NovaMapMarkerField\MapMarker;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Location extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Location::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Market';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make('Name', 'name')->sortable(),
+
+            BelongsTo::make('Customer', 'customer'),
+
+            MapMarker::make('Location')
+                ->latitude('lat')
+                ->longitude('lng')
+                ->defaultZoom(10)
+                ->hideFromIndex(),
+        ];
+    }
+
+    /**
+     * Determine if the current user can delete the given resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public function authorizedToDelete(Request $request)
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the current user can delete the given resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public function authorizedToUpdate(Request $request)
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the current user can create new resources.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public static function authorizedToCreate(Request $request)
+    {
+        return false;
+    }
+}
