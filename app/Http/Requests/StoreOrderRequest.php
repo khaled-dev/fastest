6ea2d300c469fec042d74e583d1838a687d18355
Khaled AbdElRahim <khaled710017@gmail.com>
2021-07-05 17:20:40 +0200

    [hotfix] Validate order delivery_time

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Setting;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreOrderRequest extends FormRequest
 {
@@ -23,10 +25,16 @@ class StoreOrderRequest extends FormRequest
      */
     public function rules()
     {
+        $deliveryTimes = Setting::all()->first()->delivery_time;
+
         return [
             'location_id' => 'required|exists:locations,id',
             'description' => 'required|string',
-            'delivery_time' => 'required|string',
+            'delivery_time' => [
+              'required',
+               'string',
+               Rule::in($deliveryTimes ?? [])
+            ]
         ];
     }
 }
